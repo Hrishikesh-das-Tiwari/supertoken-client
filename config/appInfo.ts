d351906fcaceb04aac6f84166a72fa250536019b
Hrishikesh A. Tiwari <hrishikeshtiwari@Hrishikeshs-MacBook-Air.local>
2023-05-02 22:20:08 +0530

    updated

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -1,36 +1,18 @@
 const port = process.env.APP_PORT || 3000;
 
 export function getApiDomain() {
-  const apiPort = process.env.REACT_APP_API_PORT || 3001;
-  const apiUrl =
-    process.env.REACT_APP_API_URL ||
-    "https://vercel.com/hrishikesh-das-tiwari/server/6HN6SwuWDSaVFS4zQwtbnGQ1c6wH";
+  const apiUrl = "https://supertoken-backend-production-a15a.up.railway.app";
   return apiUrl;
 }
 
 export function getWebsiteDomain() {
-  // let host = window.location.hostname;
-  // if (host === "localhost") {
-  //   return "http://localhost:8888";
-  // }
-
-  // let port = window.location.port;
-  // if (port !== "0" && port !== "80" && port !== "443" && port !== "") {
-  //   return "https://" + host + ":" + port;
-  // }
   return "https://supertoken-auth-frt2.vercel.app/";
 }
 
-export const websiteDomain =
-  process.env.APP_URL ||
-  process.env.NEXT_PUBLIC_APP_URL ||
-  "https://vercel.com/hrishikesh-das-tiwari/server/6HN6SwuWDSaVFS4zQwtbnGQ1c6wH";
-
 export const appInfo = {
   appName: "SuperTokens Demo App",
-  apiDomain:
-    "https://vercel.com/hrishikesh-das-tiwari/server/6HN6SwuWDSaVFS4zQwtbnGQ1c6wH",
+  apiDomain: "https://supertoken-backend-production-a15a.up.railway.app",
   websiteDomain: getWebsiteDomain(),
-  apiBasePath: "/.netlify/functions/auth",
+  apiBasePath: "/api/auth",
   websiteBasePath: "/auth",
 };
